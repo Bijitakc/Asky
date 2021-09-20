834910f719d81a09101bc92f5de039c84272d8b3
Bijitakc <30403841+Bijitakc@users.noreply.github.com>
2021-09-20 16:26:11 +0545

    added ckeditor

--- chatapp/settings.py
+++ chatapp/settings.py
@@ -135,6 +135,9 @@ AUTH_USER_MODEL="user.CustomUser"
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static'),
+# ]
 STATIC_URL = '/static/'
 MEDIA_URL='/media/'
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
