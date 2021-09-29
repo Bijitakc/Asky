3afc713669bc51156607e7d56c46d791460a3063
Bijitakc <30403841+Bijitakc@users.noreply.github.com>
2021-09-29 20:22:00 +0545

    login and signup page frontend added

--- user/admin.py
+++ user/admin.py
@@ -2,21 +2,5 @@ from django.contrib import admin
 from .models import CustomUser
 from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
 
-class CustomUserAdmin(UserAdmin):
-    model=CustomUser
-
-    fieldsets=(
-        *UserAdmin.fieldsets,
-        (
-            'Mainform',
-            {
-                'fields':(
-                    'bio',
-                    'gender'
-                )
-            }
-        )
-    )
-admin.site.register(CustomUser,CustomUserAdmin)
+admin.site.register(CustomUser)
