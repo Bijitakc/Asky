fb0b0013f9f324c2474b75f8b2a039e19f3aff44
Bijitakc <30403841+Bijitakc@users.noreply.github.com>
2021-11-05 10:52:56 +0545

    implemeted django-grappelli

--- chatapp/urls.py
+++ chatapp/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path,include
 
 urlpatterns = [
+     path('grappelli/', include('grappelli.urls')),
     path('admin/', admin.site.urls),
     path('chat/',include(('chat.urls','chat'),namespace="chat")),
     path('user/',include(('user.urls',"user"),namespace="user")),
